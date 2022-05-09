24a5c3803b4d6cbc97d4c8a82c1de78de4edb24a
mohammed el houssni <91427616+elhoussnimed@users.noreply.github.com>
2022-05-09 22:16:03 +0100

    modified some things in the script

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 let submit = document.querySelector("[type=submit]");
 let ratingParent = document.querySelector(".rating");
 let thankYou = document.querySelector(".thank-you");
-let ratingNumber = document.querySelector(".rating-number").children;
+let ratingNumber = document.querySelectorAll(".rating-number span");
 let ratingSelected = document.querySelector(".rating-selected");
 
 submit.onclick = () => {
@@ -9,10 +9,19 @@ submit.onclick = () => {
   thankYou.style.cssText = "display: block";
 };
 
-for (let i = 0; i < ratingNumber.length; i++) {
-  ratingNumber[i].onclick = function () {
-    ratingSelected.textContent = ratingNumber[i].textContent;
-    ratingNumber[i].style.cssText =
-      "background-color: var(--Orange); color: var(--white)";
-  };
-}
+// for (let i = 0; i < ratingNumber.length; i++) {
+//     ratingNumber[i].onclick = function () {
+//     ratingSelected.textContent = ratingNumber[i].textContent;
+//     ratingNumber[i].style.cssText = "background-color: var(--Orange); color: var(--white)";
+//     };
+//   };
+
+ratingNumber.forEach( e => {
+  e.onclick = () => {
+    for (let i = 0; i < ratingNumber.length; i++) {
+      ratingNumber[i].classList.remove("span-click");
+    }
+    e.classList.add("span-click");
+    ratingSelected.textContent = e.textContent;
+  }
+});
